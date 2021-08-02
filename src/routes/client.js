4a8c3d17c434bb4a0124e6eb4b2420f63652f6cb
EHER.MZA <ehermza@yahoo.com>
2021-08-01 22:00:22 -0300

    Creating the Clients class, for printing client's payments details

--- src/routes/client.js
+++ src/routes/client.js
@@ -2,16 +2,27 @@
 
 const { Router } = require("express");
 const Client = require("../models/Client");
-//const Pago = require("../models/Pago");
+const Pago = require("../models/Pago");
 
 const router = Router();
 
 router.get('/clients', async function (req, res) {
     const clients = await Client.find();
-    const arraycl = clients.map(objclient => objclient.name);
-    console.log(`Clients Array: ${arraycl} `);
-    res.render('clients', { 'clients': arraycl })
+    res.render('clients', { 'clients': clients, pagos: [], clientname: '' });
 });
+
+router.post('/clients', async function (req, res) {
+    const { idclient } = req.body;
+    let filter = { 'client': idclient }
+    const pagos = await Pago.find(filter);
+    // console.log(`Pagos: ${pagos}`);
+    const clients = await Client.find();
+
+    let filtro = { '_id': idclient }
+    const objclient = await Client.findOne(filtro);
+    const clientname = objclient.name;
+    res.render('clients', { 'clients': clients, 'pagos': pagos, clientname });
+})
 /*
     router.get('/clients/edit/:idclient', async (req, res) => {
     // Show data person of one client.
@@ -19,16 +30,16 @@ router.get('/clients', async function (req, res) {
     const cliente = await Client.find({ _id: idclient });
     res.render('profile', { container: container });
 });
- */
 router.post('/clients/edit/:idclient', async function (req, res) {
     const { idclient } = req.params;
     // console.log(`Cliente: ${req.body}`)
-
+    
     const cliente = await Client.findByIdAndUpdate(idclient, req.body);
-
+    
     console.log("Update the client to database...");
     res.redirect('/containers');
 });
+*/
 
 // router.get('/delete/:getid', async(req, res,next) => {
 //     const { getid } = req.params;
